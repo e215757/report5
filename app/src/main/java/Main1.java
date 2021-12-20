85189e5f3cf5806eac6f112be6c6d3a63adab67a
Ider Temuulen <temu@IdernoMacBook-Air.local>
2021-12-21 08:15:10 +0900

    third commmit

--- app/src/main/java/Main1.java
+++ app/src/main/java/Main1.java
@@ -1,13 +1,18 @@
 public class Main1 {
     public static void main(String[] args){
+        /*
         try{
             String str = null;
             System.out.println(str.length());
         }catch(NullPointerException e){
             System.out.println("NullPointerExceptionが発生");
             System.out.println(e.getMessage());
-            
-        }
 
         }
+        */
+
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+
+    }
 }
